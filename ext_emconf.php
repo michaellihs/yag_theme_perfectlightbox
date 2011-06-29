b6b52b83fea5a480d76a40bf4b593431cd5ab1e1
Michael Knoll <mimi@kaktusteam.de>
2011-06-29 02:25:50 +0200

    TASK: Uploaded version 0.0.1 to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "yag_theme_perfectlightbox"
+# Extension Manager/Repository config file for ext "yag_theme_perfectlightbox".
+#
+# Auto generated 29-06-2011 02:19
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -20,9 +22,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => '',
-	'uploadfolder' => '0',
+	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
@@ -33,14 +35,17 @@ $EM_CONF[$_EXTKEY] = array(
 			'cms' => '',
 			'extbase' => '',
 			'fluid' => '',
-            'perfectlightbox' => '3.1.3',
-            'yag' => '1.1.6',
+			'perfectlightbox' => '3.1.3',
+			'yag' => '1.1.6',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:10:{s:12:"ext_icon.gif";s:4:"fef0";s:14:"ext_tables.php";s:4:"6ef9";s:38:"Configuration/TypoScript/constants.txt";s:4:"30ed";s:34:"Configuration/TypoScript/setup.txt";s:4:"5f03";s:40:"Resources/Private/Language/locallang.xml";s:4:"9ede";s:42:"Resources/Private/Partials/ImageThumb.html";s:4:"08a4";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"fac1";s:31:"Resources/Public/CSS/styles.css";s:4:"5e49";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"00c0";}',
 );
 
 ?>
