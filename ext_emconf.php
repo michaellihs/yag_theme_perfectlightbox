9f360e912472a27cd1ec8f3867968b583d2989cc
Michael Knoll <mimi@kaktusteam.de>
2011-11-30 12:40:59 +0100

    CHG: Changed version to 0.0.2-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "yag_theme_perfectlightbox".
 #
-# Auto generated 29-06-2011 02:19
+# Auto generated 30-11-2011 12:33
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.0.1-dev',
+	'version' => '0.0.2-dev',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:12:"ext_icon.gif";s:4:"fef0";s:14:"ext_tables.php";s:4:"6ef9";s:38:"Configuration/TypoScript/constants.txt";s:4:"30ed";s:34:"Configuration/TypoScript/setup.txt";s:4:"5f03";s:40:"Resources/Private/Language/locallang.xml";s:4:"9ede";s:42:"Resources/Private/Partials/ImageThumb.html";s:4:"08a4";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"fac1";s:31:"Resources/Public/CSS/styles.css";s:4:"5e49";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"00c0";}',
+	'_md5_values_when_last_written' => 'a:10:{s:12:"ext_icon.gif";s:4:"fef0";s:14:"ext_tables.php";s:4:"6ef9";s:38:"Configuration/TypoScript/constants.txt";s:4:"30ed";s:34:"Configuration/TypoScript/setup.txt";s:4:"ef00";s:40:"Resources/Private/Language/locallang.xml";s:4:"9ede";s:42:"Resources/Private/Partials/ImageThumb.html";s:4:"96e9";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"7db5";s:31:"Resources/Public/CSS/styles.css";s:4:"0796";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"00c0";}',
 );
 
 ?>
